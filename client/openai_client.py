2e4af7e039776f01b7ac3dc1cea8f20e1af5474b
zzy <zyzhang_dev@163.com>
2025-04-17 23:58:33 +0800

    feat: openai_client.py 更新, 成功调用math.py

--- client/openai_client.py
+++ client/openai_client.py
@@ -1,4 +1,5 @@
 import asyncio
+import re
 
 from agents import Agent, Runner
 from agents.mcp import MCPServerStdio
@@ -7,38 +8,26 @@ from openai import OpenAI
 
 load_dotenv()  # load environment variables from .env
 
+
 def test_openai():
+    # 最原始的OpenAI调用
     client = OpenAI()
     response = client.responses.create(
-        model="gpt-4.1",
-        input="Write a one-sentence bedtime story about a unicorn."
+        model="gpt-4.1", input="Write a one-sentence bedtime story about a unicorn."
     )
     print(response.output_text)
 
+
 def test_openai_agent():
-    # 测试代码
+    # 测试OpenAI Agent
     agent = Agent(name="TestAgent", instructions="Return 'Setup successful'")
     result = Runner.run_sync(agent, "Run test")
     print(result.final_output)  # Expected output: "Setup successful"
 
 
-async def main():
-    # agent not use mcp
-    agent_without_mcp = Agent(
-        name="Assistant",
-        model="gpt-4o-mini",
-        instructions="Solve Math Problems.",
-    )
-    result_without_mcp = Runner.run_streamed(
-        starting_agent=agent_without_mcp, input="which one is larger, 9.11 or 9.3?"
-    )
-    print("the result of agent-without-mcp is: ")
-    async for event in result_without_mcp.stream_events():
-        if event.type == "raw_response_event" and isinstance(
-            event.data, ResponseTextDeltaEvent
-        ):
-            print(event.data.delta, end="", flush=True)
-
+async def test_openai_agent_with_mcp():
+    # 测试OpenAI Agent with MCP Server
+    samples_dir = "～/Desktop"
     async with MCPServerStdio(
         params={
             "command": "npx",
@@ -48,7 +37,6 @@ async def main():
         # List tools provided by the MCP server
         tools = await server.list_tools()
 
-        samples_dir = "～/Desktop"
         # agent that uses the MCP server
         agent_with_mcp = Agent(
             name="Assistant",
@@ -62,6 +50,43 @@ async def main():
         print("the result of agent-with-mcp is: ", result_with_mcp.final_output)
 
 
+async def main():
+    # agent not use mcp
+    agent_without_mcp = Agent(
+        name="Assistant",
+        model="gpt-4o-mini",
+        instructions="Solve Math Problems.",
+    )
+    result_without_mcp = await Runner.run(
+        starting_agent=agent_without_mcp, input="compare which one is larger, 9.11 or 9.3?"
+    )
+    print("the result of agent-without-mcp is: ", result_without_mcp.final_output)
+
+    # agent use mcp
+    async with MCPServerStdio(
+        params={
+            "command": "uv",
+            "args": ["--directory", "../server", "run", "math.py"],
+        }
+    ) as server:
+        # List tools provided by the MCP server
+        tools = await server.list_tools()
+
+        # agent that uses the MCP server
+        agent_with_mcp = Agent(
+            name="Assistant",
+            model="gpt-4o-mini",
+            instructions="Solve Math Problems.",
+            mcp_servers=[server],
+        )
+        result_with_mcp = await Runner.run(
+            starting_agent=agent_with_mcp, input="compare which one is larger, 9.11 or 9.3?"
+        )
+        print("the result of agent-with-mcp is: ", result_with_mcp.final_output)
+
+
 if __name__ == "__main__":
-    # asyncio.run(main())
-    test_openai()
+    # test_openai()
+    # test_openai_agent()
+    # asyncio.run(test_openai_agent_with_mcp())
+    asyncio.run(main())
